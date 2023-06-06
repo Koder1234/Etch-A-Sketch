66f1aabd5cb4e778d512ba35184d6fa84ab15bb8
Koder1234 <130999355+Koder1234@users.noreply.github.com>
2023-06-06 12:03:40 +0200

    add code for generating blocks

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const container = document.querySelector("#container");
+const clicker = document.querySelector("button");
 let squares 
-for (let i=0;i<16*16;i++)
-{
-    squares=document.createElement("div");
-    squares.setAttribute("class","placeholder")
-    squares.textContent="xd";
+console.log(clicker);
 
-    container.appendChild(squares);
+
+clicker.addEventListener("click",()=>{
+    let squareAmount=prompt("How many squares do you want to generate",1);
+    generateSquares(squareAmount);
+})
+
+
+function generateSquares(amount)
+{
+    remover=document.querySelectorAll(".placeholder")
+    for(node of remover)
+    {
+    container.removeChild(document.querySelector(".placeholder"));
+    }
+    for (let i=0;i<amount;i++)
+    {
+        squares=document.createElement("div");
+        squares.setAttribute("class","placeholder");
+        squares.textContent="xd";
+        container.appendChild(squares);
+    } 
 }
-console.log(squares);
-container.appendChild(squares);
+
+generateSquares(16*16);
+console.log(document.querySelectorAll(".placeholder"));
+
+
+
+//console.log(squares);
+//container.appendChild(squares);
