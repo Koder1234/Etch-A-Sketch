dba3ea4fd6a732c5a881193fac58ad02ff52e96b
Koder1234 <130999355+Koder1234@users.noreply.github.com>
2023-06-06 17:23:06 +0200

    fix generating elemnets so that they take the same amount of place

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const container = document.querySelector("#container");
 const clicker = document.querySelector("button");
+const containerStyle = getComputedStyle(container);
+const containerWidth=containerStyle.width;
+const number= containerWidth.split("p")
 let squares 
-console.log(clicker);
 
 
 clicker.addEventListener("click",()=>{
     let squareAmount=prompt("How many squares do you want to generate",1);
     generateSquares(squareAmount);
 })
-
 
 function generateSquares(amount)
 {
@@ -17,19 +18,35 @@ function generateSquares(amount)
     {
     container.removeChild(document.querySelector(".placeholder"));
     }
-    for (let i=0;i<amount;i++)
+    for (let i=0;i<amount*amount;i++)
     {
         squares=document.createElement("div");
         squares.setAttribute("class","placeholder");
+        
+
         squares.textContent="xd";
         container.appendChild(squares);
     } 
+ 
+    document.querySelectorAll(".placeholder").forEach(element => {
+        element.style.width=`${(number[0]-3*amount-1)/amount}px`;
+        element.style.height=`${(number[0]-3*amount-1)/amount}px`;
+        
+    });
+
+
 }
 
-generateSquares(16*16);
-console.log(document.querySelectorAll(".placeholder"));
+// zmienna.addEventListener("hover",function(){
+//     console.log(zmienna);
 
 
+// })
 
-//console.log(squares);
-//container.appendChild(squares);
+generateSquares(16);
+
+//let zmienna=document.getElementsByClassName(".placeholder");
+console.log(squares)
+//console.log(`${number[0]/20}`);
+
+
