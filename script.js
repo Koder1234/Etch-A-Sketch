9d4312910c9c20863593790a151caf07554e69fe
Koder1234 <130999355+Koder1234@users.noreply.github.com>
2023-06-07 17:04:41 +0200

    working Etch-A-Sketch. Add efect of coloring a tile upon hovering on it.

--- script.js
+++ script.js
@@ -2,18 +2,23 @@ const container = document.querySelector("#container");
 const clicker = document.querySelector("button");
 const containerStyle = getComputedStyle(container);
 const containerWidth=containerStyle.width;
-const number= containerWidth.split("p")
-let squares 
+const number= containerWidth.split("p")// get the width of the container
+let squares; 
 
 
 clicker.addEventListener("click",()=>{
     let squareAmount=prompt("How many squares do you want to generate",1);
+    while(squareAmount>100)
+    {
+        squareAmount=prompt("The number of must be lower then 100, plese input the number",1);
+    }
     generateSquares(squareAmount);
 })
 
 function generateSquares(amount)
 {
-    remover=document.querySelectorAll(".placeholder")
+    
+    remover=document.querySelectorAll(".placeholder")  //deletes all the squares for genereating new ones
     for(node of remover)
     {
     container.removeChild(document.querySelector(".placeholder"));
@@ -22,31 +27,32 @@ function generateSquares(amount)
     {
         squares=document.createElement("div");
         squares.setAttribute("class","placeholder");
-        
-
-        squares.textContent="xd";
         container.appendChild(squares);
     } 
- 
     document.querySelectorAll(".placeholder").forEach(element => {
         element.style.width=`${(number[0]-3*amount-1)/amount}px`;
         element.style.height=`${(number[0]-3*amount-1)/amount}px`;
         
     });
-
-
+   
+    elements=document.querySelectorAll(".placeholder");
+    let array=Array.from(elements);
+    for(element of array)
+    {
+        element.addEventListener("mouseover",(element)=>
+        {        
+            element.target.classList.add("colorChange");            
+        })
+    }
 }
-
-// zmienna.addEventListener("hover",function(){
-//     console.log(zmienna);
-
-
-// })
 
 generateSquares(16);
 
-//let zmienna=document.getElementsByClassName(".placeholder");
-console.log(squares)
-//console.log(`${number[0]/20}`);
+
+
+
+
+
+
 
 
